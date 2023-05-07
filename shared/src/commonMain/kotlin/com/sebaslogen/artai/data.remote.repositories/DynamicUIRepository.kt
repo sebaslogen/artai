beb3c4b88abe3d1151aa165641f9474ba761132e
sebaslogen <neoranga55@gmail.com>
2023-05-07 15:02:13 +0200

    Update to coroutines 1.7.0 and use Dispatchers.IO in KMM

--- shared/src/commonMain/kotlin/com/sebaslogen/artai/data.remote.repositories/DynamicUIRepository.kt
+++ shared/src/commonMain/kotlin/com/sebaslogen/artai/data.remote.repositories/DynamicUIRepository.kt
@@ -2,17 +2,18 @@ package com.sebaslogen.artai.data.remote.repositories
 
 import com.sebaslogen.artai.data.remote.api.DynamicUIApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 class DynamicUIRepository(private val dynamicUIApi: DynamicUIApi) {
-    suspend fun home() = withContext(Dispatchers.Default) { // TODO: Add IO dispatcher
+    suspend fun home() = withContext(Dispatchers.IO) {
         dynamicUIApi.home() // TODO: Map to domain model
         // TODO: Add error handling to return success or error
     }
 
-    suspend fun homeReloaded() = withContext(Dispatchers.Default) { // TODO: Add IO dispatcher
+    suspend fun homeReloaded() = withContext(Dispatchers.IO) {
         dynamicUIApi.homeReloaded() // TODO: Map to domain model
         // TODO: Add error handling to return success or error
     }
