e22ec25d3a605e3f70bc2ff4356c1db30eb9db09
sebaslogen <neoranga55@gmail.com>
2023-06-13 10:07:59 +0200

    Fix ktorfit deprectation

--- shared/src/commonMain/kotlin/com/sebaslogen/artai/di/components/ApiServicesComponent.kt
+++ shared/src/commonMain/kotlin/com/sebaslogen/artai/di/components/ApiServicesComponent.kt
@@ -4,7 +4,7 @@ import com.sebaslogen.artai.data.remote.api.DynamicUIApi
 import com.sebaslogen.artai.di.scopes.Singleton
 import com.sebaslogen.artai.networking.Http
 import de.jensklingenberg.ktorfit.Ktorfit
-import de.jensklingenberg.ktorfit.converter.builtin.CallResponseConverter
+import de.jensklingenberg.ktorfit.converter.builtin.CallConverterFactory
 import de.jensklingenberg.ktorfit.ktorfit
 import me.tatarka.inject.annotations.Provides
 
@@ -13,7 +13,7 @@ interface ApiServicesComponent {
     @Provides
     fun ktorfit(http: Http): Ktorfit = ktorfit {
         httpClient(http)
-        responseConverter(CallResponseConverter())
+        converterFactories(CallConverterFactory())
         baseUrl("https://raw.githubusercontent.com/")
     }
 
