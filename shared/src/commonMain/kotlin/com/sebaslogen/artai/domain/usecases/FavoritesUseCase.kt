bc526e1f329dd6ef5782e668d78c59063a294f86
sebaslogen <neoranga55@gmail.com>
2023-08-07 14:50:41 +0200

    Add fav use case and currying

--- shared/src/commonMain/kotlin/com/sebaslogen/artai/domain/usecases/FavoritesUseCase.kt
+++ shared/src/commonMain/kotlin/com/sebaslogen/artai/domain/usecases/FavoritesUseCase.kt
@@ -7,5 +7,26 @@ import me.tatarka.inject.annotations.Inject
 class FavoritesUseCase(
     private val favoritesRepository: FavoritesRepository
 ) {
+    val favorites = favoritesRepository.favorites
+    private fun setFavorite(id: String, favorited: Boolean) {
+        val favorites = favoritesRepository.favorites.value
+        val alreadyFavorited = favorites.contains(id)
+        val addFavorite = favorited && !alreadyFavorited
+        val removeFavorite = !favorited && alreadyFavorited
+        if (addFavorite) {
+            favoritesRepository.updateFavorites(favorites + id)
+        } else if (removeFavorite) {
+            favoritesRepository.updateFavorites(favorites - id)
+        }
+    }
 
+    /**
+     * Returns a function that can only update the favorited state of the given favorite [id].
+     * This is called currying.
+     */
+    fun favoriteSetter(id: String): (Boolean) -> Unit {
+        return fun(favorited: Boolean) {
+            setFavorite(id, favorited)
+        }
+    }
 }
