02c1cb08e3ee01001cfcf904fb9d5521f380650c
sebaslogen <neoranga55@gmail.com>
2023-09-21 15:57:29 +0200

    Add navigztion for favsVM option

--- shared/src/commonMain/kotlin/com/sebaslogen/artai/domain/Navigator.kt
+++ shared/src/commonMain/kotlin/com/sebaslogen/artai/domain/Navigator.kt
@@ -1,7 +1,16 @@
 package com.sebaslogen.artai.domain
 
 import com.sebaslogen.artai.domain.models.Action
+import io.github.aakira.napier.Napier
 
 interface Navigator {
     fun onNavigation(action: Action.OpenScreen)
+}
+
+abstract class NotANavigatorHandler(private val name: String) : Navigator {
+    override fun onNavigation(action: Action.OpenScreen) {
+        Napier.e {
+            "Class $name cannot handle navigation actions. Received action: $action"
+        }
+    }
 }
